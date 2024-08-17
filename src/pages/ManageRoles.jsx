fdb45a41592c7788acbe2bd96874caae57be53e8
Ayesha Maryam <ayesham.fos1505@gmail.com>
2024-08-17 22:59:42 +0500

    role updated

--- src/pages/ManageRoles.jsx
+++ src/pages/ManageRoles.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FaPlus, FaTrash, FaEdit } from "react-icons/fa";
+import axios from "axios";
 
 const ManageRoles = () => {
+  const [roles, setRoles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchRoles = async () => {
+      try {
+        const response = await axios.get("http://localhost:5001/roles");
+        setRoles(response.data);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchRoles();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  if (!Array.isArray(roles)) {
+    return <p>No Roles Available.</p>;
+  }
+
   return (
     <div className="w-full">
       <div className="mx-20 my-9 bg-blue-950 bg-opacity-5 rounded-lg py-3 px-5">
         <div className="flex justify-between items-center">
-          <h1 className="text-2xl font-bold py-3">Manage User Roles</h1>
+          <h1 className="text-2xl font-bold py-3">User Roles</h1>
           <Link to={"/create_role"}>
             <button
               type="button"
@@ -31,124 +57,31 @@ const ManageRoles = () => {
                   Description
                 </th>
                 <th scope="col" className="px-6 py-3">
-                  Status
-                </th>
-                <th scope="col" className="px-6 py-3">
                   Action
                 </th>
               </tr>
             </thead>
             <tbody>
-              <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                <td className="px-6 py-4">1</td>
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  Admin
-                </th>
-                <td className="px-6 py-4">Lorem ipsum dolor sit amet, consectetur adipisicing.</td>
-                <td className="px-6 py-4">Default</td>
-                <td className="px-6 py-4">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+              {roles.map((role) => (
+                <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                  <td className="px-6 py-4">{role.id}</td>
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                   >
-                    <div className="flex gap-2">
-                      <FaEdit />
-                      <FaTrash className="text-red-600" />
+                    {role.role_name}
+                  </th>
+                  <td className="px-6 py-4">
+                    {role.description}
+                  </td>
+                  <td className="px-6 py-4">
+                    <div className="flex gap-2 font-medium  hover:underline">
+                      <FaEdit className="text-blue-600 dark:text-blue-500" />
+                      <FaTrash className="text-red-600 dark:text-red-500" />
                     </div>
-                  </a>
-                </td>
-              </tr>
-              <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                <td className="px-6 py-4">2</td>
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  Admin
-                </th>
-                <td className="px-6 py-4">Lorem ipsum dolor sit amet, consectetur adipisicing.</td>
-                <td className="px-6 py-4">Default</td>
-                <td className="px-6 py-4">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
-                    <div className="flex gap-2">
-                      <FaEdit />
-                      <FaTrash className="text-red-600" />
-                    </div>
-                  </a>
-                </td>
-              </tr>
-              <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                <td className="px-6 py-4">3</td>
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  Admin
-                </th>
-                <td className="px-6 py-4">Lorem ipsum dolor sit amet, consectetur adipisicing.</td>
-                <td className="px-6 py-4">Default</td>
-                <td className="px-6 py-4">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
-                    <div className="flex gap-2">
-                      <FaEdit />
-                      <FaTrash className="text-red-600" />
-                    </div>
-                  </a>
-                </td>
-              </tr>
-              <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                <td className="px-6 py-4">4</td>
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  Admin
-                </th>
-                <td className="px-6 py-4">Lorem ipsum dolor sit amet, consectetur adipisicing.</td>
-                <td className="px-6 py-4">Default</td>
-                <td className="px-6 py-4">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
-                    <div className="flex gap-2">
-                      <FaEdit />
-                      <FaTrash className="text-red-600" />
-                    </div>
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td className="px-6 py-4">5</td>
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  Admin
-                </th>
-                <td className="px-6 py-4">Lorem ipsum dolor sit amet, consectetur adipisicing.</td>
-                <td className="px-6 py-4">Default</td>
-                <td className="px-6 py-4">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
-                    <div className="flex gap-2">
-                      <FaEdit />
-                      <FaTrash className="text-red-600" />
-                    </div>
-                  </a>
-                </td>
-              </tr>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
